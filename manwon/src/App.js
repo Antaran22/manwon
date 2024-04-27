85be66890a869aea2621191b2c3a427d5e52ffd4
antaran <1031105mj@gmail.com>
2024-04-27 15:05:24 +0900

    first commit

---
+++ manwon/src/App.js
@@ -0,0 +1,10 @@
+import Team from "./pages/Team";
+function App() {
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ manwon/src/pages/Team.jsx
@@ -0,0 +1,12 @@
+import List from '../components/List'
+import React from 'react'
+
+function Team() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Team
