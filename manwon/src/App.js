f2df45e29a4b4330f6b6c8b2a2b23e67807919ff
antaran <1031105mj@gmail.com>
2024-05-06 13:44:29 +0900

    SellerHome 페이지 구현 완료

--- manwon/src/App.js
+++ manwon/src/App.js
@@ -1,9 +1,10 @@
 import Team from "./pages/Team";
+
 function App() {
   return (
-    <div>
-      
-    </div>
+    <>
+    <Team/>
+    </>
   );
 }
 
--- manwon/src/pages/Team.jsx
+++ manwon/src/pages/Team.jsx
@@ -3,9 +3,18 @@ import React from 'react'
 
 function Team() {
   return (
-    <div>
-      
-    </div>
+    <>
+       <article>
+        <section className={"list"}>
+          <h1>만원 프로젝트</h1>
+          <ol>
+            {[ "1팀", "2팀", "3팀", "4팀", "5팀", "6팀", "7팀", "8팀", "9팀", "10팀", "11팀", "12팀", "13팀", "14팀", "15팀", "16팀", "17팀", "18팀",].map((list) => {
+              return <List key={list} list={list} />;
+            })}
+          </ol>
+        </section>
+      </article>
+    </>
   )
 }
 
